6d563bf73909dba42eb3b70b520de5152257f5f7
Niklas Kihlberg <niklaskihlberg@me.com>
2025-06-25 17:38:58 +0200

    Initial Commit

---
+++ JoyPanTlt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JoyPanTlt
+//
+//  Created by Niklas Kihlberg on 2025-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JoyPanTlt/JoyPanTltApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyPanTltApp.swift
+//  JoyPanTlt
+//
+//  Created by Niklas Kihlberg on 2025-06-25.
+//
+
+import SwiftUI
+
+@main
+struct JoyPanTltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
